4ea4d130f8ff395f25b9dc2f54c08669c4c9a17d
SH <shomenko1510@gmail.com>
2026-07-12 11:45:33 +0300

    Add initial SQL database structure and DimDirectionscripts

---
+++ inserts/03_Insert_DimDirection.sql
@@ -0,0 +1,36 @@
+/*
+============================================================================================================
+Project : Financial Analytics SQL
+Script  : 03_Insert_DimDirection.sql
+Purpose : Populate the DimDirection table
+Author  : Sergii Khomenko
+============================================================================================================
+*/
+
+USE FinanceAnalyticsPortfolioDB;
+GO
+
+INSERT INTO dbo.DimDirection
+(
+    DirectionName,
+    SortOrder
+)
+VALUES
+    ('Business Line A', 1),
+    ('Business Line B', 2),
+    ('Business Line C', 3),
+    ('Business Line D', 4),
+    ('Business Line E', 5),
+    ('Business Line F', 6),
+    ('Business Line G', 7),
+    ('Business Line H', 8),
+    ('Other', 9);
+GO    
+
+SELECT
+    DirectionId,
+    DirectionName,
+    SortOrder
+FROM dbo.DimDirection
+ORDER BY SortOrder;
+GO    
