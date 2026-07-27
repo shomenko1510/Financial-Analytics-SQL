64f21943f83556c2e343d5ca679e2b19a71c5aea
SH <shomenko1510@gmail.com>
2026-07-27 09:01:11 +0300

    Complete FactSales table structure

---
+++ database/14_Add_FactSales_Constraints.sql
@@ -0,0 +1,39 @@
+USE FinanceAnalyticsPortfolioDB;
+GO 
+
+ALTER TABLE dbo.FactSales
+ADD CONSTRAINT FK_FactSales_DimCompany
+FOREIGN KEY (CompanyId)
+REFERENCES dbo.DimCompany (CompanyId);
+GO
+
+ALTER TABLE dbo.FactSales
+ADD CONSTRAINT FK_FactSales_DimPeriod
+FOREIGN KEY (PeriodId)
+REFERENCES dbo.DimPeriod (PeriodId);
+GO
+
+ALTER TABLE dbo.FactSales
+ADD CONSTRAINT FK_FactSales_DimScenario
+FOREIGN KEY (ScenarioId)
+REFERENCES dbo.DimScenario (ScenarioId);
+GO
+
+ALTER TABLE dbo.FactSales
+ADD CONSTRAINT FK_FactSales_DimDirection
+FOREIGN KEY (DirectionId)
+REFERENCES dbo.DimDirection (DirectionId);
+GO
+
+ALTER TABLE dbo.FactSales
+ADD CONSTRAINT UQ_FactSales
+UNIQUE
+(
+        CompanyId,
+        PeriodId,
+        ScenarioId,
+        DirectionId
+);
+GO
+
+
