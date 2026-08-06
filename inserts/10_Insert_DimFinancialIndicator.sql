7d30c994d1ce942e142022b5599f8418317a6a2f
Sergii Khomenko <shomenko1510@gmail.com>
2026-08-06 14:51:39 +0300

    Updated DimFinancialIndicator and factSales Q2 scripts

--- inserts/10_Insert_DimFinancialIndicator.sql
+++ inserts/10_Insert_DimFinancialIndicator.sql
@@ -20,7 +20,7 @@ INSERT INTO dbo.DimFinancialIndicator
 ) 
 VALUES
     /* Cash balances */
-    ('CASH_OPEN',        'Opening Cash Balance',                'Cash Flow',            'ths. UAN',     1),
+    ('CASH_OPEN',        'Opening Cash Balance',                'Cash Flow',            'ths. UAH',     1),
     /* Income Statement */
     ('REV_EX_VAT',       'Revenue (excl. VAT)',                 'Income Statement',     'ths. UAH',     2),
     ('COGS_EX_VAT',      'COGS (excl. VAT)',                    'Income Statement',     'ths. UAH',     3),
@@ -37,22 +37,14 @@ VALUES
     ('PBT',              'Profit Before Tax',                   'Income Statement',     'ths. UAH',     14),
     ('INCOME_TAX',       'Income Tax',                          'Income Statement',     'ths. UAH',     15),
     ('NP',               'Net Profit',                          'Income Statement',     'ths. UAH',     16),
-    
-    /*Working Capital(Balance Sheet)*/
-    ('INV',              'Total Inventory',                     'Balance Sheet',        'ths. UAN',     33),
-    ('AR',               'Total Trade Receivables',             'Balance Sheet',        'ths. UAN',     34),
-    ('AP',               'Total Payables',                      'Balance Sheet',        'ths. UAN',     35),
-    
     /* Operating cash flow */
     ('CHG_INV',          'Change in Inventory',                 'Cash Flow',            'ths. UAH',     17),
     ('CHG_AR',           'Change in Trade Receivables',         'Cash Flow',            'ths. UAH',     18),
     ('CHG_AP',           'Change in Trade Payables',            'Cash Flow',            'ths. UAH',     19),
     ('OCF',              'Operating Cash Flow',                 'Cash Flow',            'ths. UAH',     20),
-    
     /* Investing activities */
     ('CAPEX',            'Capital Expenditures',                'Cash Flow',            'ths. UAH',     21),
     ('CF_INV',           'Cash Flow from Investing Activities', 'Cash Flow',            'ths. UAH',     22),
-    
     /* Financing activities */
     ('SHARE_CAP',        'Charge in Share Capital',             'Cash Flow',            'ths. UAH',     23),
     ('TARGET_FIN',       'Target Financing',                    'Cash Flow',            'ths. UAH',     24),
@@ -62,7 +54,6 @@ VALUES
     ('BORROWINGS_REPAY', 'Repayment of Borrowings',             'Cash Flow',            'ths. UAH',     28),
     ('FIN_OTHER',        'Other',                               'Cash Flow',            'ths. UAH',     29),
     ('CF_FIN',           'Cash Flow from Financing Activities', 'Cash Flow',            'ths. UAH',     30),
-    
     /* Cash movement and closing balance */
     ('NCF',              'Net Cash Flow',                       'Cash Flow',            'ths. UAH',     31),
     ('CASH_CLOSE',       'Closing Cash Balance',                'Cash Flow',            'ths. UAH',     32);
--- inserts/15_Insert_FactSales_Actual_2023_Q2.sql
+++ inserts/15_Insert_FactSales_Actual_2023_Q2.sql
@@ -14,7 +14,7 @@ DECLARE @PeriodId INT;
 DECLARE @ScenarioId INT;
 
 SELECT @CompanyId = CompanyId
-FROM dbo.Company
+FROM dbo.DimCompany
 WHERE CompanyName = 'Company A';
 
 SELECT @ScenarioId = ScenarioId
@@ -24,7 +24,7 @@ WHERE ScenarioName = 'Actual';
 SELECT @PeriodId = PeriodId
 FROM dbo.DimPeriod
 WHERE [Year] = 2023
-    AND [Quorter] = 'Q2';
+    AND [Quarter] = 'Q2';
 
 --Check whether the data has already been loaded    
 IF EXISTS
@@ -44,41 +44,71 @@ BEGIN
     );
     RETURN;
 END;
-;WITH SouceData AS
+;WITH SourceData AS
+(
     SELECT *
     FROM
-(
-    VALUES
-        ('Business Line A',   729.4500,    73.1000,   53322.8000),
-        ('Business Line B',   343.7200,   216.4000,   52741.9800),
-        ('Business Line C', 11737.2700,    15.8000,  185495.0000),
-        ('Business Line D',     0.0000,     0.0000,       0.0000),
-        ('Business Line E',   172.0200,    63.2800,   10884.9700),
-        ('Business Line F',   256.4500,    37.7700,    9686.2600),
-        ('Business Line G',   150.1600,    27.2800,    4095.8000),
-        ('Business Line H',    21.7800,    35.4000,     771.0300),
-        ('Other',               1.0000,  4837.2400,    4837.2400) 
-) AS V     
+    (
+        VALUES
+            ('Business Line A',   729.4500,    73.1000,   53322.8000),
+            ('Business Line B',   343.7200,   216.4000,   52741.9800),
+            ('Business Line C', 11737.2700,    15.8000,  185495.0000),
+            ('Business Line D',     0.0000,     0.0000,       0.0000),
+            ('Business Line E',   172.0200,    63.2800,   10884.9700),
+            ('Business Line F',   256.4500,    37.7700,    9686.2600),
+            ('Business Line G',   150.1600,    27.2800,    4095.8000),
+            ('Business Line H',    21.7800,    35.4000,     771.0300),
+            ('Other',               1.0000,  4837.2400,    4837.2400) 
+    ) AS V     
 
+    (
+        DirectionName,
+        Volume,
+        Price,
+        RevenueAmount
+    )
+)
+--Check Total Revenue.
+/*
+SELECT SUM(RevenueAmount)
+FROM SourceData AS SD;
+*/ 
+ -- Check difference 
+/*
+SELECT 
+    SD.DirectionNane,
+    SD.Volume,
+    SD.Price,
+    SD.RevenueAmount,
+    SD.Volume * SD.Price AS RevenueCalcualtion,
+    SD.RevenueAmount - (SD.Volume * SD.Price) AS Difference  
+FROM SourceData AS SD
+ORDER BY DirectionName;
+*/
+
+INSERT INTO dbo.FactSales
 (
-    DirectionName,
+    CompanyId, 
+    PeriodId,
+    ScenarioId,
+    DirectionID,
     Volume,
     Price,
     RevenueAmount
 )
-
---Check Total Revenue.
-
-SELECT 
-    SD.DiractionNane,
+SELECT
+    @CompanyId,
+    @PeriodId,
+    @ScenarioId,
+    D.DirectionId,
     SD.Volume,
     SD.Price,
-    SD.RevenueAmount,
-    SD.Volume * SD.Price AS RevenueCalcualtion
-    SD.RevenueAmount - (SD.Volume * SD.Price) AS Difference  
-FROM SourseData AS SD
-ORDER BY DirectionName
+    SD.RevenueAmount
+FROM SourceData AS SD
+INNER JOIN dbo.DimDirection AS D
+ON SD.DirectionName = D.DirectionName;    
 
+PRINT 'FactSales Actual 2023 Q2 inserted succesfully';
 
 
 
