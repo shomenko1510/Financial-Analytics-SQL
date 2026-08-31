68536535f2b2eb4ea2179eee2dd321414551e91b
Sergii Khomenko <shomenko1510@gmail.com>
2026-08-31 15:43:05 +0300

    Completed FactSales Actual and Budget data for 2023-2025

--- inserts/29_ETL_StgSales_Budget_to_FactSales.sql
+++ inserts/29_ETL_StgSales_Budget_to_FactSales.sql
@@ -56,4 +56,36 @@ INNER JOIN dbo.DimPeriod AS P
     AND SS.[Quarter] = P.[Quarter]
 
 INNER JOIN dbo.DimScenario AS S
-    ON SS.         
+    ON SS.ScenarioName = S.ScenarioName
+
+INNER JOIN dbo.DimDirection AS D
+    ON SS.DirectionName = D.DirectionName
+
+WHERE SS.CompanyName = 'Company A'
+    AND SS.ScenarioName = 'Budget'
+    AND SS.[Year] BETWEEN 2023 AND 2025;
+
+ SELECT
+    P.[Year],
+    P.[Quarter],
+    S.ScenarioName,
+    COUNT(*) AS CountRows,
+    SUM(FS.RevenueAmount) AS TotalRevenue
+FROM dbo.FactSales AS FS
+
+INNER JOIN dbo.DimPeriod AS P
+    ON FS.PeriodId = P.PeriodId
+
+INNER JOIN dbo.DimScenario AS S
+    ON FS.ScenarioId = S.ScenarioId
+
+WHERE S.ScenarioName = 'Budget'
+    AND P.[Year] BETWEEN 2023 AND 2025
+
+GROUP BY
+    P.[Year],
+    P.[Quarter],
+    S.ScenarioName
+ORDER BY
+    P.[Year],
+    P.Quarter;             
