922922373ee5a295e96480875e028b06afe3aa57
Sergii Khomenko <shomenko1510@gmail.com>
2026-09-01 15:52:01 +0300

    Loaded Actual StgOPEX data for Company A, 2023 - 2025 from CSV file

--- database/23_Create_StgCOGS.sql
+++ database/23_Create_StgCOGS.sql
@@ -12,15 +12,11 @@ GO
 
 CREATE TABLE dbo.StgCOGS
 (
-    StgCOGSId      INT IDENTITY(1,1) NOT NULL,
     CompanyName    NVARCHAR(100) NOT NULL,
     [Year]         INT NOT NULL,
     [Quarter]      NVARCHAR(2) NOT NULL,
     ScenarioName   NVARCHAR(50) NOT NULL,
     DirectionName  NVARCHAR(100) NOT NULL,
-    COGSAmount     DECIMAL(18,4) NOT NULL,
-
-    CONSTRAINT PK_StgCOGS
-        PRIMARY KEY (StgCOGSId)
+    COGSAmount     DECIMAL(18,4) NOT NULL 
 );
 GO
---
+++ inserts/31_Load_StgOPEX_Actual_From_CSV.sql
@@ -0,0 +1,48 @@
+USE FinanceAnalyticsPortfolioDB;
+GO
+
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.StgOPEX
+    WHERE CompanyName = 'Company A'
+    AND ScenarioName = 'Actual'
+    AND [Year] BETWEEN 2023 AND 2025
+)
+BEGIN
+    RAISERROR
+    (
+        'Actual StgOPEX data for Company A, 2023 - 2025 already exists',
+        16,
+        1
+    )
+    RETURN;
+END;
+
+BULK INSERT dbo.StgOPEX
+FROM 'D:\GitHub\Financial-Analytics-SQL\data\OPEX_Actual_SQL.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0a',
+    CODEPAGE = '65001',
+    TABLOCK
+);
+GO
+
+SELECT
+    [Year],
+    [Quarter],
+    ScenarioName,
+    COUNT(*)
+FROM dbo.StgOPEX
+WHERE ScenarioName = 'Actual'
+GROUP BY
+    [Year],
+    [Quarter],
+    ScenarioName
+ORDER BY
+    [Year],
+    [Quarter];         
+
