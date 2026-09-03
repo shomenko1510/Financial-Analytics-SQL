4545fcca246e323f2c7da8df9f7920ae9e8b4a24
Sergii Khomenko <shomenko1510@gmail.com>
2026-09-03 12:00:58 +0300

    Added StgWorkingCapital Budget data for 2023 - 2025 from csv file

---
+++ inserts/33_Load_StgWorkingCapital_Actual_From_CSV.sql
@@ -0,0 +1,50 @@
+USE FinanceAnalyticsPortfolioDB;
+GO
+
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.StgWorkingCapital
+    WHERE CompanyName = 'Company A'
+        AND ScenarioName = 'Budget'
+        AND [Year] BETWEEN 2023 AND 2025
+)
+BEGIN
+    RAISERROR
+    (
+        'StgWorkingCapital Actual for 2023-2025 already exists.',
+        16,
+        1
+    );
+    RETURN;
+END;
+
+BULK INSERT dbo.StgWorkingCapital
+FROM 'D:\GitHub\Financial-Analytics-SQL\data\WorkingCapital_Budget_SQL.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0a',
+    CODEPAGE = '65001',
+    TABLOCK
+);
+GO
+
+SELECT
+    [Year],
+    [Quarter],
+    ScenarioName,
+    COUNT(*) AS CountRows
+FROM dbo.StgWorkingCapital
+
+WHERE ScenarioName = 'Budget'
+
+GROUP BY
+    [Year],
+    [Quarter],
+    ScenarioName
+
+ORDER BY
+    [Year],
+    [Quarter];
