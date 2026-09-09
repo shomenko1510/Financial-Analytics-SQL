cbb5ebc15070ee64ec69bf878b5ef3f813336558
Sergii Khomenko <shomenko1510@gmail.com>
2026-09-09 16:17:29 +0300

    Loaded Budget StgFinancialData for Company A, 2023-2026 from csv file

---
+++ inserts/44_Load_StgFinancialData_Budget_From_CSV.sql
@@ -0,0 +1,58 @@
+USE FinanceAnalyticsPortfolioDB;
+GO
+
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.StgFinancialData
+    WHERE CompanyName = 'Company A'
+        AND ScenarioName = 'Budget'
+        AND [Year] BETWEEN 2023 AND 2025       
+)
+BEGIN
+    RAISERROR
+    (
+        'StgFinancialData Budget data for Company A, 2023 - 2025 already exists',
+        16,
+        1
+    );
+END
+ELSE
+BEGIN
+
+    BULK INSERT dbo.StgFinancialData
+    FROM 'D:\GitHub\Financial-Analytics-SQL\data\Cash_Flow_Budget_SQL.csv'
+    WITH
+    (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        ROWTERMINATOR = '0x0a',
+        CODEPAGE = '65001',
+        TABLOCK
+    );
+END;
+GO
+
+SELECT
+    [Year],
+    [Quarter],
+    ScenarioName,
+    COUNT(*) AS CountRows 
+FROM dbo.StgFinancialData
+
+WHERE ScenarioName = 'Budget'
+
+GROUP BY
+    [Year],
+    [Quarter],
+    ScenarioName
+ORDER BY
+    [Year],
+    [QUarter],
+    ScenarioName;     
+
+
+
+
+
+    
